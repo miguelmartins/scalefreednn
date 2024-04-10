fb4797ddafd153f4429fa9a4e69761ef3bc15e57
Miguel Martins <miguelopesmartins@gmail.com>
2024-04-10 14:55:54 +0100

    Fix baseline binary segmentation metrics

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -9,11 +9,26 @@ def specificity(y_true, y_pred):
 
 
 def dice_coefficient(y_true, y_pred, epsilon=1e-12):
+    print("Shape of input", y_true.shape)
+    print(y_true)
     # adapted from: https://stackoverflow.com/questions/72195156/correct-implementation-of-dice-loss-in-tensorflow-keras
     _y_true = tf.keras.layers.Flatten()(y_true)
     _y_pred = tf.where(tf.keras.layers.Flatten()(y_pred) >= 0.5, 1., 0.)
-    intersection = tf.reduce_sum(_y_pred * _y_true)
-    return (2. * intersection + epsilon) / (tf.reduce_sum(_y_true, ) + tf.reduce_sum(_y_pred) + epsilon)
+    intersection = tf.reduce_sum(_y_pred * _y_true, axis=-1)
+    return (2. * intersection + epsilon) / (tf.reduce_sum(_y_true, axis=-1) + tf.reduce_sum(_y_pred, axis=-1) + epsilon)
+
+
+def dice_coefficient_multi(y_true, y_pred, epsilon=1e-12):
+    shape_ = y_true.shape
+    _y_true = tf.keras.layers.Reshape([shape_[1] * shape_[2], shape_[3]])(y_true)
+    _y_pred = tf.where(tf.keras.layers.Reshape([shape_[1] * shape_[2], shape_[3]])(y_pred) >= 0.5, 1., 0)
+    intersection = tf.reduce_sum(_y_pred * _y_true, axis=1)
+    return (2. * intersection + epsilon) / (
+                tf.reduce_sum(_y_true, axis=1) + tf.reduce_sum(_y_pred, axis=1) + epsilon)
+
+
+def avg_dice_coefficient_multi(y_true, y_pred, epsilon=1e-12):
+    return tf.reduce_mean(dice_coefficient_multi(y_true, y_pred, epsilon), axis=-1)
 
 
 def get_baseline_segmentation_metrics():
@@ -21,7 +36,11 @@ def get_baseline_segmentation_metrics():
     auc = tf.keras.metrics.AUC()
     prec = tf.keras.metrics.Precision()
     sens = tf.keras.metrics.Recall(name='sensitivity')
-    spec = specificity
-    iou = tf.keras.metrics.BinaryIoU()
-    # dice = tf.keras.metrics.F1Score(name='dice')
-    return [accuracy, auc, prec, sens, spec, dice_coefficient, iou]
+    spec = tf.keras.metrics.MeanMetricWrapper(specificity, name='specificity')
+    dice_score = tf.keras.metrics.MeanMetricWrapper(fn=dice_coefficient, name='dice_score')
+    iou = tf.keras.metrics.BinaryIoU(num_classes=2, target_class_ids=[1])
+    return [accuracy, auc, prec, sens, spec, dice_score, iou]
+
+
+def get_baseline_segmentation_metrics_multi():
+    return [avg_dice_coefficient_multi]
--- optimization/loss_functions.py
+++ optimization/loss_functions.py
@@ -1,17 +1,39 @@
 import tensorflow as tf
 
 
-def dice_loss(epsilon=1e-12):
+# def dice_loss(epsilon=1e-12):  # DOES NOT ACCOUNT FOR TF.SUM_BATCH REDUCTION
+#     def soft_dice_coefficient(y_true, y_pred):
+#         _y_true = tf.keras.layers.Flatten()(y_true)
+#         _y_pred = tf.keras.layers.Flatten()(y_pred)
+#         intersection = tf.reduce_sum(_y_pred * _y_true)
+#         soft_dice = (2. * intersection + epsilon) / (tf.reduce_sum(_y_true) + tf.reduce_sum(_y_pred) + epsilon)
+#         return 1. - soft_dice
+#     return soft_dice_coefficient
+
+def dice_loss(epsilon=1e-12):  # ASSUMES TF.SUM_OVER_BATCH REDUCTION
     def soft_dice_coefficient(y_true, y_pred):
-        _y_true = tf.keras.layers.Flatten()(y_true)
+        _y_true = tf.keras.layers.Flatten()(y_true)  # B x H x W x C -> B x (H.W.C)
         _y_pred = tf.keras.layers.Flatten()(y_pred)
-        intersection = tf.reduce_sum(_y_pred * _y_true)
-        soft_dice = (2. * intersection + epsilon) / (tf.reduce_sum(_y_true, ) + tf.reduce_sum(_y_pred) + epsilon)
-        return 1. - soft_dice
+        intersection = tf.reduce_sum(_y_pred * _y_true, axis=-1)  # B x (H.W.C) -> B
+        soft_dice = (2. * intersection + epsilon) / (tf.reduce_sum(_y_true, axis=-1) + tf.reduce_sum(_y_pred, axis=-1) + epsilon)
+        return tf.reduce_mean(1. - soft_dice)  # B -> 1  # AS IN TF.SUM_OVER_BATCH SIZE REDUCTION
     return soft_dice_coefficient
 
 
-def binary_weighted_loss(alpha, loss1, loss2):
+def dice_loss_multi(epsilon=1e-12):  # todo: CHECK https://github.com/Beckschen/TransUNet/blob/main/utils.py
+    def soft_dice_coefficient_multi(y_true, y_pred):
+        shape_ = y_true.shape
+        _y_true = tf.keras.layers.Reshape([shape_[1] * shape_[2], shape_[3]])(y_true)
+        _y_pred = tf.keras.layers.Reshape([shape_[1] * shape_[2], shape_[3]])(y_pred)
+        intersection = tf.reduce_sum(_y_pred * _y_true, axis=1)
+        soft_dice = (2. * intersection + epsilon) / (tf.reduce_sum(_y_true, axis=1) + tf.reduce_sum(_y_pred, axis=1) + epsilon)
+        soft_dice_loss = 1. - soft_dice
+        avg_soft_dice_loss = tf.reduce_mean(soft_dice_loss, axis=-1)
+        return tf.reduce_sum(avg_soft_dice_loss, axis=0) / shape_[0] # SUM_OVER_BATCH REDUCTION
+    return soft_dice_coefficient_multi
+
+
+def weighted_loss(alpha, loss1, loss2):
     def loss(y_true, y_pred):
         return alpha * loss1(y_true, y_pred) + (1 - alpha) * loss2(y_true, y_pred)
     return loss
